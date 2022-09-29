965d9f087e4b8a7cef4119306884a43803fe324a
AnnaNesterenko <nura_arh@ukr.net>
2022-09-29 12:59:41 +0300

    addBaseName

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       {/* basename="/goit-react-hw-05-movies/" */}
       <App />
     </BrowserRouter>
